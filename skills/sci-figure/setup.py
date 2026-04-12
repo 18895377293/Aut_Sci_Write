f7be1329eec3c622f52b5ef04220945a8e35712f
Shuo Zhao <2720828562@qq.com>
2026-04-13 02:18:48 +0800

    fix problem

--- skills/sci-figure/setup.py
+++ skills/sci-figure/setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setup(
-    name='Sh_Sci_Fig',
+    name='sci-figure',
     version='0.1.0',
     description='Scientific figure extractor for academic PDF papers',
     long_description=long_description,
